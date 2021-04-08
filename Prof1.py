ec58042c03c83bba0fdcf7c347e588d590cef651
Nicobossio42 <Nicolasvergon123@gmail.com>
2021-04-07 19:28:08 -0500

    Profe, aun tengo dudas para que me imprima cuando le respondo que no, pero pues esta en un 95% hecho

---
+++ Prof1.py
@@ -0,0 +1,36 @@
+Inicio = str("""A continuacion vera tres alternativas de carrera, debera escoger una
+              y decir si quiere escuchar una reseña: 
+              1 - Ingeneria de sistemas
+              2 - Diseño Grafico
+              3 - Comunicacion social """)
+print(Inicio)
+option = input("¿Que opcion desea escoger? ")
+si = True
+no = False
+
+if option == "1":
+    print("¿Desea escuchar una reseña?")
+elif si:
+    print("")
+elif no:
+    print("Gracias por su cooperacion")
+
+if option == "2":
+    print("¿Desea escuchar una reseña?")
+    input()
+elif si:
+    print("")
+elif no:
+    print("Gracias por su cooperacion")
+
+if option == "3":
+    print("¿Desea escuchar una reseña?")
+    input()
+elif si:
+    print("")
+elif no:
+    print("Gracias por su cooperacion")
+
+
+
+
